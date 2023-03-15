c3881f4ae53714acfcc88a45d6017e6f1c5746b1
schepkej <38034933+jschepke@users.noreply.github.com>
2023-03-15 21:45:57 +0100

    Changed function description

--- src/features/auth/AuthObserver.tsx
+++ src/features/auth/AuthObserver.tsx
@@ -13,7 +13,7 @@ interface AuthObserverProps {
  * This component checks the state of the authentication and its loading status.
  *
  * @remarks
- * Use to check if the user is logged in and gets him to a dashboard or login form.
+ * Checks if the user is logged in and gets him to a dashboard or login form.
  *
  * @returns Progress bar when auth is unset and navigates to a dashboard if logged in.
  * Otherwise returns children eg. login/register form.
