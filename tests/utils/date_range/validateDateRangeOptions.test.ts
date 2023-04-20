ec2117c8d68b0ba21d7aaee79b577a98edcd559c
schepkej <38034933+jschepke@users.noreply.github.com>
2023-04-20 23:32:57 +0200

    Refactor tests in validateDateRangeOptions.test.ts
    Improve block descriptions and structure of tests in the file.

--- tests/utils/date_range/validateDateRangeOptions.test.ts
+++ tests/utils/date_range/validateDateRangeOptions.test.ts
@@ -7,6 +7,7 @@ import {
 } from '../../../src/utils/date_range/dateRange';
 import { validateDateRangeOptions } from '../../../src/utils/date_range/validateDateRangeOptions';
 import { isNumber } from '../../../src/utils/isNumber';
+import { isObject } from '../../../src/utils/isObject';
 import { isValidWeekday } from '../../../src/utils/isValidWeekday';
 
 describe('validateDateRangeOptions', () => {
@@ -25,102 +26,127 @@ describe('validateDateRangeOptions', () => {
     { invalidInput: false },
   ];
 
-  describe('general input value', () => {
-    test.each(invalidInputValues)(
-      'throws error if value is $invalidInput',
+  describe('Given no input', () => {
+    test('throws an error if input is not specified', () => {
+      // @ts-expect-error: testing invalid input
+      expect(() => validateDateRangeOptions()).toThrowError();
+    });
+  });
+
+  describe('Given a non-object as input', () => {
+    const nonObjectValues = invalidInputValues.filter(
+      (value) => !isObject(value.invalidInput)
+    );
+    test.each(nonObjectValues)(
+      'throws an error if input is $invalidInput',
       ({ invalidInput }) => {
         expect(() => validateDateRangeOptions(invalidInput)).toThrowError();
       }
     );
-    test('throws error if unknown property is specified', () => {
-      expect(() => validateDateRangeOptions({ test: 'test' })).toThrowError();
-      expect(() =>
-        validateDateRangeOptions({ test: 'test', refDate: 'test' })
-      ).toThrowError();
+  });
+
+  describe('Given an object with unexpected properties', () => {
+    test.each([{ test: 'test' }, { test: 'test', refDate: 'test' }])(
+      'throws an error if input is %o ',
+      (object) => {
+        expect(() => validateDateRangeOptions(object)).toThrowError();
+      }
+    );
+  });
+
+  describe('Given object with expected properties', () => {
+    describe('refDate property', () => {
+      describe('Given a valid refDate', () => {
+        // values
+        const validRefDateValues = [
+          { description: 'current Date: new Date()', value: new Date() },
+          {
+            description: 'specific Date: new Date(2021, 11, 25)',
+            value: new Date(2021, 11, 25),
+          },
+          {
+            description: 'current DateTime: DateTime.now()',
+            value: DateTime.now(),
+          },
+          {
+            description: 'specific DateTime: DateTime.fromISO("2021-12-25")',
+            value: DateTime.fromISO('2021-12-25'),
+          },
+        ];
+
+        // tests
+        test.each(validRefDateValues)(
+          `doesn't throw error if refDate is $description`,
+          (item) => {
+            expect(() =>
+              validateDateRangeOptions({ refDate: item.value })
+            ).not.toThrowError();
+          }
+        );
+      });
+
+      describe('Given a non valid refDate', () => {
+        // filter valid refDate values
+        const filter = invalidInputValues.filter(
+          (item) => item.invalidInput !== undefined
+        );
+
+        // add invalid values specific to refDate
+        const invalidRefDateValues = [
+          { invalidInput: new Date('invalid') },
+          { invalidInput: DateTime.invalid('invalid') },
+          ...filter,
+        ];
+
+        test.each(invalidRefDateValues)(
+          'throws an error if refDate is $invalidInput',
+          ({ invalidInput: invalidValue }) => {
+            expect(() =>
+              validateDateRangeOptions({ refDate: invalidValue })
+            ).toThrowError();
+          }
+        );
+      });
     });
   });
 
-  describe('refDate value', () => {
-    // filter valid refDate values
-    const filter = invalidInputValues.filter(
-      (item) => item.invalidInput !== undefined
-    );
+  describe('refWeekday property', () => {
+    describe('Given a valid refWeekday', () => {
+      // test for all valid values
+      test.each([1, 2, 3, 4, 5, 6, 7])(
+        "doesn't throw error if refWeekday is %d",
+        (num) => {
+          expect(() => validateDateRangeOptions({ refWeekday: num }));
+        }
+      );
+    });
 
-    // add invalid values specific to refDate
-    const invalidRefDateValues = [
-      { invalidInput: new Date('invalid') },
-      { invalidInput: DateTime.invalid('invalid') },
-      ...filter,
-    ];
+    describe('Given a non valid refWeekday', () => {
+      // filter out valid refWeekday values
+      const filter = invalidInputValues.filter(
+        (item) =>
+          !isValidWeekday(item.invalidInput) && item.invalidInput !== undefined
+      );
+      // add invalid values specific to refWeekday
+      const invalidRefWeekdayValues = [
+        { invalidInput: -1 },
+        { invalidInput: 0 },
+        { invalidInput: 8 },
+        ...filter,
+      ];
 
-    const validRefDateValues = [
-      { description: 'current Date: new Date()', value: new Date() },
-      {
-        description: 'specific Date: new Date(2021, 11, 25)',
-        value: new Date(2021, 11, 25),
-      },
-      {
-        description: 'current DateTime: DateTime.now()',
-        value: DateTime.now(),
-      },
-      {
-        description: 'specific DateTime: DateTime.fromISO("2021-12-25")',
-        value: DateTime.fromISO('2021-12-25'),
-      },
-    ];
-
-    test.each(invalidRefDateValues)(
-      'throws error if refDate is $invalidInput',
-      ({ invalidInput: invalidValue }) => {
-        expect(() =>
-          validateDateRangeOptions({ refDate: invalidValue })
-        ).toThrowError();
-      }
-    );
-
-    test.each(validRefDateValues)(
-      `doesn't throw error if value is $description`,
-      (item) => {
-        expect(() =>
-          validateDateRangeOptions({ refDate: item.value })
-        ).not.toThrowError();
-      }
-    );
+      test.each(invalidRefWeekdayValues)(
+        'throws an error if refWeekday is $invalidInput',
+        ({ invalidInput }) => {
+          expect(() =>
+            validateDateRangeOptions({ refWeekday: invalidInput })
+          ).toThrowError();
+        }
+      );
+    });
   });
 
-  describe('refWeekday value', () => {
-    // filter valid refWeekday values
-    const filter = invalidInputValues.filter(
-      (item) =>
-        !isValidWeekday(item.invalidInput) && item.invalidInput !== undefined
-    );
-    // add invalid values specific to refWeekday
-    const invalidRefWeekdayValues = [
-      { invalidInput: -1 },
-      { invalidInput: 0 },
-      { invalidInput: 8 },
-      ...filter,
-    ];
-
-    test.each(invalidRefWeekdayValues)(
-      'throws error if refWeekday is $invalidInput',
-      ({ invalidInput }) => {
-        expect(() =>
-          validateDateRangeOptions({ refWeekday: invalidInput })
-        ).toThrowError();
-      }
-    );
-
-    // test for all valid values
-    test.each([1, 2, 3, 4, 5, 6, 7])(
-      "doesn't throw error if refWeekday is %d",
-      (num) => {
-        expect(() => validateDateRangeOptions({ refWeekday: num }));
-      }
-    );
-  });
-
-  describe('offset value', () => {
+  describe('Offset properties', () => {
     const invalidOffsetValues = invalidInputValues.filter(
       (item) =>
         !(isNumber(item.invalidInput) && item.invalidInput > 0) &&
@@ -129,139 +155,151 @@ describe('validateDateRangeOptions', () => {
 
     const validOffsetValues = [1, 123];
 
-    describe('startOffset', () => {
-      test.each(invalidOffsetValues)(
-        'throws error if startOffset value is $invalidInput',
-        ({ invalidInput }) => {
-          expect(() =>
-            validateDateRangeOptions({ startOffset: invalidInput })
-          ).toThrowError();
-        }
-      );
+    describe('startOffset property', () => {
+      describe('Given a valid startOffset', () => {
+        test.each(validOffsetValues)(
+          "doesn't throw error if startOffset is %d",
+          (num) => {
+            expect(() =>
+              validateDateRangeOptions({ startOffset: num })
+            ).not.toThrowError();
+          }
+        );
+      });
 
-      test.each(validOffsetValues)(
-        "doesn't throw error if startOffset is %d",
-        (num) => {
-          expect(() =>
-            validateDateRangeOptions({ startOffset: num })
-          ).not.toThrowError();
-        }
-      );
+      describe('Given a non valid startOffset', () => {
+        test.each(invalidOffsetValues)(
+          'throws an error if startOffset is $invalidInput',
+          ({ invalidInput }) => {
+            expect(() =>
+              validateDateRangeOptions({ startOffset: invalidInput })
+            ).toThrowError();
+          }
+        );
+      });
     });
 
-    describe('endOffset', () => {
-      test.each(invalidOffsetValues)(
-        'throws error if endOffset value is $invalidInput',
-        ({ invalidInput }) => {
-          expect(() =>
-            validateDateRangeOptions({ endOffset: invalidInput })
-          ).toThrowError();
-        }
-      );
-
-      test.each(validOffsetValues)(
-        "doesn't throw error if endOffset is %d",
-        (num) => {
-          expect(() =>
-            validateDateRangeOptions({ endOffset: num })
-          ).not.toThrowError();
-        }
-      );
+    describe('endOffset property', () => {
+      describe('Given a valid endOffset', () => {
+        test.each(validOffsetValues)(
+          "doesn't throw error if endOffset is %d",
+          (num) => {
+            expect(() =>
+              validateDateRangeOptions({ endOffset: num })
+            ).not.toThrowError();
+          }
+        );
+      });
+      describe('Given a non valid endOffset', () => {
+        test.each(invalidOffsetValues)(
+          'throws an error if endOffset is $invalidInput',
+          ({ invalidInput }) => {
+            expect(() =>
+              validateDateRangeOptions({ endOffset: invalidInput })
+            ).toThrowError();
+          }
+        );
+      });
     });
   });
 
-  describe('mixed valid and invalid values', () => {
-    // some valid inputs
-    const validInputs: DateRangeOptions[] = [
-      {
-        refDate: DateTime.now(),
-        refWeekday: Weekday.Saturday,
-        endOffset: 3,
-        startOffset: 1,
-      },
-      {
-        refWeekday: Weekday.Monday,
-        startOffset: 2,
-      },
-      {
-        refDate: new Date(2021, 11, 25), // replaced with JS Date object
-        endOffset: 5,
-      },
-      {
-        endOffset: 0,
-        startOffset: 0,
-      },
-      {
-        refDate: DateTime.now().plus({ days: 2 }),
-        refWeekday: Weekday.Friday,
-      },
-      {
-        refDate: new Date(2022, 0, 1), // replaced with JS Date object
-        refWeekday: Weekday.Sunday,
-        endOffset: 4,
-        startOffset: 2,
-      },
-      {
-        refWeekday: Weekday.Wednesday,
-        endOffset: 3,
-      },
-      {
-        refDate: DateTime.now().minus({ days: 2 }),
-        startOffset: 1,
-      },
-    ];
-    const invalidInputs = [
-      {
-        refDate: DateTime.fromISO('invalid'), // invalid DateTime
-        refWeekday: Weekday.Saturday,
-        endOffset: 3,
-        startOffset: 1,
-      },
-      {
-        refWeekday: Weekday.Monday,
-        startOffset: -1, // invalid offset
-      },
-      {
-        refDate: new Date(`test`), // invalid Date
-        endOffset: 5,
-      },
-      {
-        endOffset: '1', // invalid offset
-        startOffset: 3,
-      },
-      {
-        refDate: DateTime.now().plus({ days: 2 }),
-        refWeekday: 'friday', // invalid
-      },
-      {
-        refDate: new Date(2022, 0, 1),
-        refWeekday: null, // invalid
-        endOffset: 4,
-        startOffset: null, // invalid
-      },
-      {
-        refWeekday: 9, // invalid
-        endOffset: 3,
-      },
-      {
-        refDate: DateTime.now().minus({ days: 2 }),
-        startOffset: [], // invalid
-      },
-    ];
+  describe('Mixed properties of the input object', () => {
+    describe('Given an object with mixed expected properties with valid values', () => {
+      // some valid inputs
+      const validInputs: DateRangeOptions[] = [
+        {
+          refDate: DateTime.now(),
+          refWeekday: Weekday.Saturday,
+          endOffset: 3,
+          startOffset: 1,
+        },
+        {
+          refWeekday: Weekday.Monday,
+          startOffset: 2,
+        },
+        {
+          refDate: new Date(2021, 11, 25), // replaced with JS Date object
+          endOffset: 5,
+        },
+        {
+          endOffset: 0,
+          startOffset: 0,
+        },
+        {
+          refDate: DateTime.now().plus({ days: 2 }),
+          refWeekday: Weekday.Friday,
+        },
+        {
+          refDate: new Date(2022, 0, 1), // replaced with JS Date object
+          refWeekday: Weekday.Sunday,
+          endOffset: 4,
+          startOffset: 2,
+        },
+        {
+          refWeekday: Weekday.Wednesday,
+          endOffset: 3,
+        },
+        {
+          refDate: DateTime.now().minus({ days: 2 }),
+          startOffset: 1,
+        },
+      ];
 
-    test.each(validInputs)(
-      `doesn't throw errors or return anything for %s`,
-      (input) => {
-        expect(() => validateDateRangeOptions(input)).not.toThrowError();
-        expect(validateDateRangeOptions(input)).toBeUndefined();
-      }
-    );
+      test.each(validInputs)(
+        `doesn't throw errors or return anything for %s`,
+        (input) => {
+          expect(() => validateDateRangeOptions(input)).not.toThrowError();
+          expect(validateDateRangeOptions(input)).toBeUndefined();
+        }
+      );
+    });
 
-    test.each(invalidInputs)(
-      `throw errors if invalid options mixed with valid ones: %s`,
-      (input) => {
-        expect(() => validateDateRangeOptions(input)).toThrowError();
-      }
-    );
+    describe('Given an object with mixed expected and unexpected properties', () => {
+      const invalidInputs = [
+        {
+          refDate: DateTime.fromISO('invalid'), // invalid DateTime
+          refWeekday: Weekday.Saturday,
+          endOffset: 3,
+          startOffset: 1,
+        },
+        {
+          refWeekday: Weekday.Monday,
+          startOffset: -1, // invalid offset
+        },
+        {
+          refDate: new Date(`test`), // invalid Date
+          endOffset: 5,
+        },
+        {
+          endOffset: '1', // invalid offset
+          startOffset: 3,
+        },
+        {
+          refDate: DateTime.now().plus({ days: 2 }),
+          refWeekday: 'friday', // invalid
+        },
+        {
+          refDate: new Date(2022, 0, 1),
+          refWeekday: null, // invalid
+          endOffset: 4,
+          startOffset: null, // invalid
+        },
+        {
+          refWeekday: 9, // invalid
+          endOffset: 3,
+        },
+        {
+          refDate: DateTime.now().minus({ days: 2 }),
+          startOffset: [], // invalid
+        },
+      ];
+
+      test.each(invalidInputs)(
+        `throws an errors if unexpected property is provided: %s`,
+        (input) => {
+          expect(() => validateDateRangeOptions(input)).toThrowError();
+        }
+      );
+    });
   });
 });
