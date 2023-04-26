7e0cba564cd2621bb255f84da1242008613b1e77
schepkej <38034933+jschepke@users.noreply.github.com>
2023-04-26 02:18:01 +0200

    refactor: isNumber tsdoc description
    Improved description of the function.

--- src/utils/isNumber.ts
+++ src/utils/isNumber.ts
@@ -1,8 +1,10 @@
 /**
- * A utility function that checks if the input is a valid number
+ * A utility function that checks if the input is a number, including integers and decimals.
+ *
+ * @remarks The function doesn't check for integers
  *
  * @param input - The input to be checked
- * @returns A boolean indicating whether the input is a valid number
+ * @returns A boolean indicating whether the input is a integer or decimal number.
  */
 export function isNumber(input: unknown): input is number {
   return typeof input === 'number' && Number.isFinite(input);
